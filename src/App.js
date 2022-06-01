87f62f80b20f75d570a21af37dfa8f36ea41c197
dwi13L <dw22197@gmail.com>
2022-06-01 23:31:08 +0530

    clean up for recipe project base

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import React, { useState } from "react";
-import CounterHooks from "./CounterHooks";
-import Counter from "./Counter";
-
-export const ThemeContext = React.createContext(undefined);
+import React from "react";
 
 function App() {
-  const [theme, setTheme] = useState("red");
-
-  return (
-    <ThemeContext.Provider value={{ backgroundColor: theme }}>
-      <h2>Counter 1</h2>
-      <Counter initialCount={0} />
-      <br />
-      <h2>Counter 2</h2>
-      <CounterHooks initialCount={0} />
-      <br />
-      <button
-        onClick={() => {
-          setTheme((prevTheme) => (prevTheme === `red` ? `blue` : `red`));
-        }}
-      >
-        Toggle theme
-      </button>
-    </ThemeContext.Provider>
-  );
+  return null;
 }
 
 export default App;
--- src/ThemeContext.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const ThemeContext = React.createContext("grey");
-const ThemeProvider = ThemeContext.Provider;
-const ThemeConsumer = ThemeContext.Consumer;
-
-export { ThemeProvider, ThemeConsumer };
-export default ThemeContext;
